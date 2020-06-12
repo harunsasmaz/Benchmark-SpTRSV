13b21af3e6dbcf59d2553f83b3701afaf95724bd
harunsasmaz <hsasmaz16@ku.edu.tr>
2020-06-12 14:31:35 +0300

    works correctly under v100 but glop is too high

--- SpTRSV_cuda/sptrsv_syncfree_cuda.h
+++ SpTRSV_cuda/sptrsv_syncfree_cuda.h
@@ -406,6 +406,77 @@ void sptrsm_syncfree_cuda_executor_update(const int* __restrict__        d_cscCo
     }
 }
 
+__global__
+void sptrsv_zerocopy_cuda_analyser(const int   *d_cscRowIdx,
+                                   const int    m,
+                                   const int    nnz,
+                                         int   *s_in_degree)
+{
+    const int global_id = blockIdx.x * blockDim.x + threadIdx.x; //get_global_id(0);
+    if (global_id < nnz)
+    {
+        atomicAdd(&s_in_degree[d_cscRowIdx[global_id]], 1);
+    }
+}
+
+__global__
+void sptrsv_zerocopy_cuda_executor(const int* __restrict__        d_cscColPtr,
+                                   const int* __restrict__        d_cscRowIdx,
+                                   const VALUE_TYPE* __restrict__ d_cscVal,
+                                         int*                     d_in_degree,
+                                         VALUE_TYPE*              d_left_sum,
+                                   const int                      n,
+                                   const int                      displs,
+                                   const VALUE_TYPE* __restrict__ d_b,
+                                         VALUE_TYPE*              d_x,
+                                         int*                     s_in_degree,
+                                         VALUE_TYPE*              s_left_sum)
+{
+    const int global_id = blockIdx.x * blockDim.x + threadIdx.x;
+    int global_x_id = global_id / WARP_SIZE;
+    if (global_x_id >= n) return;
+ 
+    const int local_warp_id = threadIdx.x / WARP_SIZE;
+    const int lane_id = (WARP_SIZE - 1) & threadIdx.x;
+    int starting_x = displs;
+
+    clock_t start;
+    do {
+        start = clock();
+        __syncwarp();
+    }
+    while (s_in_degree[global_x_id] != d_in_degree[global_x_id] + 1);
+    
+    const int pos = d_cscColPtr[global_x_id];
+    const VALUE_TYPE coef = (VALUE_TYPE)1 / d_cscVal[pos];
+    VALUE_TYPE xi = d_left_sum[global_x_id] + s_left_sum[global_x_id];
+    xi = (d_b[global_x_id] - xi) * coef;
+
+    const int start_ptr = d_cscColPtr[global_x_id] + 1;
+    const int stop_ptr  = d_cscColPtr[global_x_id + 1];
+
+    for (int j = start_ptr + lane_id; j < stop_ptr; j += WARP_SIZE)
+    {
+        const int rowIdx = d_cscRowIdx[j];
+        const bool cond = (rowIdx < starting_x + n);
+
+        if (cond) {
+            const int pos = rowIdx - starting_x;
+
+            atomicAdd((VALUE_TYPE *)&d_left_sum[pos], xi * d_cscVal[j]);
+            __threadfence();
+            atomicAdd((int *)&d_in_degree[pos], 1);
+        }
+        else {
+            atomicAdd(&s_left_sum[rowIdx], xi * d_cscVal[j]);
+            //__threadfence();
+            atomicSub(&s_in_degree[rowIdx], 1);
+        }
+    }
+
+    if (!lane_id) d_x[global_x_id] = xi;
+}
+
 int sptrsv_syncfree_cuda(const int           *cscColPtrTR,
                          const int           *cscRowIdxTR,
                          const VALUE_TYPE    *cscValTR,
@@ -597,77 +668,6 @@ int sptrsv_syncfree_cuda(const int           *cscColPtrTR,
     return 0;
 }
 
-__global__
-void sptrsv_zerocopy_cuda_analyser(const int   *d_cscRowIdx,
-                                   const int    m,
-                                   const int    nnz,
-                                         int   *s_in_degree)
-{
-    const int global_id = blockIdx.x * blockDim.x + threadIdx.x; //get_global_id(0);
-    if (global_id < nnz)
-    {
-        atomicAdd(&s_in_degree[d_cscRowIdx[global_id]], 1);
-    }
-}
-
-__global__
-void sptrsv_zerocopy_cuda_executor(const int* __restrict__        d_cscColPtr,
-                                   const int* __restrict__        d_cscRowIdx,
-                                   const VALUE_TYPE* __restrict__ d_cscVal,
-                                         int*                     d_in_degree,
-                                         VALUE_TYPE*              d_left_sum,
-                                   const int                      n,
-                                   const int                      displs,
-                                   const VALUE_TYPE* __restrict__ d_b,
-                                         VALUE_TYPE*              d_x,
-                                         int*                     s_in_degree,
-                                         VALUE_TYPE*              s_left_sum)
-{
-    const int global_id = blockIdx.x * blockDim.x + threadIdx.x;
-    int global_x_id = global_id / WARP_SIZE;
-    if (global_x_id >= n) return;
- 
-    const int local_warp_id = threadIdx.x / WARP_SIZE;
-    const int lane_id = (WARP_SIZE - 1) & threadIdx.x;
-    int starting_x = displs;
-
-    clock_t start;
-    do {
-        start = clock();
-        __syncwarp();
-    }
-    while (s_in_degree[global_x_id] != d_in_degree[global_x_id] + 1);
-    
-    const int pos = d_cscColPtr[global_x_id];
-    const VALUE_TYPE coef = (VALUE_TYPE)1 / d_cscVal[pos];
-    VALUE_TYPE xi = d_left_sum[global_x_id] + s_left_sum[global_x_id];
-    xi = (d_b[global_x_id] - xi) * coef;
-
-    const int start_ptr = d_cscColPtr[global_x_id] + 1;
-    const int stop_ptr  = d_cscColPtr[global_x_id + 1];
-
-    for (int j = start_ptr + lane_id; j < stop_ptr; j += WARP_SIZE)
-    {
-        const int rowIdx = d_cscRowIdx[j];
-        const bool cond = (rowIdx < starting_x + n);
-
-        if (cond) {
-            const int pos = rowIdx - starting_x;
-
-            atomicAdd((VALUE_TYPE *)&d_left_sum[pos], xi * d_cscVal[j]);
-            __threadfence();
-            atomicAdd((int *)&d_in_degree[pos], 1);
-        }
-        else {
-            atomicAdd(&s_left_sum[rowIdx], xi * d_cscVal[j]);
-            //__threadfence();
-            atomicSub(&s_in_degree[rowIdx], 1);
-        }
-    }
-
-    if (!lane_id) d_x[global_x_id] = xi;
-}
-
 int sptrsv_zerocopy_cuda(const int           *cscColPtrTR,
                          const int           *cscRowIdxTR,
                          const VALUE_TYPE    *cscValTR,
@@ -687,6 +687,7 @@ int sptrsv_zerocopy_cuda(const int           *cscColPtrTR,
     int ngpu = 1; //cudaGetDeviceCount();
     
     // calculate which GPU gets which cols
+    // almost even col distribution
     int colCounts[ngpu], colDispls[ngpu];
     int colCount = round((double)n / ngpu);
     for(int i = 0; i < ngpu - 1; i++)
@@ -713,14 +714,14 @@ int sptrsv_zerocopy_cuda(const int           *cscColPtrTR,
     cudaMemset(s_left_sum, 0, n * sizeof(int));
     cudaMemset(s_in_degree, 0, n * sizeof(int));
 
-    // transfer host mem to device mem
+    // Define partial device arrays for matrix L and vectors X and B
     int *d_cscColPtrTR[ngpu];
     int *d_cscRowIdxTR[ngpu];
     VALUE_TYPE *d_cscValTR[ngpu];
     VALUE_TYPE *d_b[ngpu];
     VALUE_TYPE *d_x[ngpu];
 
-    // Allocate partitioned matrix L on devices
+    // Allocate partial device arrays for matrix L on devices
     for(int i = 0; i < ngpu; i++)
     {
         cudaSetDevice(i);
@@ -739,7 +740,7 @@ int sptrsv_zerocopy_cuda(const int           *cscColPtrTR,
         cudaMemcpy(d_cscValTR[i], cscValTR + e_pos, elms * sizeof(VALUE_TYPE), cudaMemcpyHostToDevice);
     }
 
-    // Allocate vector B
+    // Allocate and distribute vector B on devices
     for(int i = 0; i < ngpu; i++)
     {   
         int pos = colDispls[i], cols = colCounts[i];
@@ -748,7 +749,7 @@ int sptrsv_zerocopy_cuda(const int           *cscColPtrTR,
         cudaMemcpy(d_b[i], b + pos, cols * sizeof(VALUE_TYPE), cudaMemcpyHostToDevice);
     }
 
-    // Allocate vector X
+    // Allocate and set vector X on devices
     for(int i = 0; i < ngpu; i++)
     {   
         int pos = colDispls[i], cols = colCounts[i];
@@ -757,13 +758,11 @@ int sptrsv_zerocopy_cuda(const int           *cscColPtrTR,
         cudaMemset(d_x[i], 0, cols * sizeof(VALUE_TYPE));
     }
 
-    //  - cuda zercopu SpTRSV analysis start!
-    printf(" - cuda zerocopy SpTRSV analysis start!\n");
-
+    //  Define device in_degree and left_sum arrays for each device
     int *d_in_degree[ngpu];
     VALUE_TYPE *d_left_sum[ngpu];
 
-    // Allocate local in_degree and left_sum arrays
+    // Allocate device in_degree and left_sum arrays for each device
     for(int i = 0; i < ngpu; i++)
     {
         int cols = colCounts[i];
@@ -774,23 +773,32 @@ int sptrsv_zerocopy_cuda(const int           *cscColPtrTR,
         cudaMemset(d_in_degree[i], 0, cols * sizeof(int));
     }
 
-    // For safety, wait until all allocations are ready
+    // For safety, wait until all allocations are done.
     for(int i = 0; i < ngpu; i++)
     {
         cudaSetDevice(i);
         cudaDeviceSynchronize();
     }
 
+    //  - cuda zercopu SpTRSV analysis start!
+    printf(" - cuda zerocopy SpTRSV analysis start!\n");
+
+    struct timeval t1, t2;
+    gettimeofday(&t1, NULL);
+
     int num_threads = 128;
     int num_blocks;
 
-    // BENCHMARK is not added until one iteration works correctly.
-    for(int i = 0; i < ngpu; i++)
+    for(int k = 0; k < BENCH_REPEAT; k++)
     {
-        cudaSetDevice(i);
-        num_blocks = ceil((double)eCounts[i] / num_threads);
-        sptrsv_zerocopy_cuda_analyser<<< num_blocks, num_threads >>>
-                                    (d_cscRowIdxTR[i], colCounts[i], eCounts[i], s_in_degree);
+        cudaMemset(s_in_degree, 0, n * sizeof(int));
+        for(int i = 0; i < ngpu; i++)
+        {
+            cudaSetDevice(i);
+            num_blocks = ceil((double)eCounts[i] / num_threads);
+            sptrsv_zerocopy_cuda_analyser<<< num_blocks, num_threads >>>
+                                        (d_cscRowIdxTR[i], colCounts[i], eCounts[i], s_in_degree);
+        }
     }
 
     // For safety, wait until are devices are done with analysis.
@@ -800,28 +808,59 @@ int sptrsv_zerocopy_cuda(const int           *cscColPtrTR,
         cudaDeviceSynchronize();
     }
 
+    gettimeofday(&t2, NULL);
+    double time_cuda_analysis = (t2.tv_sec - t1.tv_sec) * 1000.0 + (t2.tv_usec - t1.tv_usec) / 1000.0;
+    time_cuda_analysis /= BENCH_REPEAT;
+
+    printf("cuda zerocopy SpTRSV analysis on L used %4.2f ms\n", time_cuda_analysis);
+
     //  - cuda syncfree SpTRSV solve start!
     printf(" - cuda zerocopy SpTRSV solve start!\n");
 
     num_threads = WARP_PER_BLOCK * WARP_SIZE;
+    double time_cuda_solve = 0;
 
-    // BENCHMARK is not added until one loop works.
-    for(int i = 0; i < ngpu; i++)
+    // For benchmarking, backup intermediate in degree array
+    int *s_in_degree_backup;
+    cudaMallocManaged((void**)&s_in_degree_backup, n * sizeof(int));
+    memcpy(s_in_degree_backup, s_in_degree, n * sizeof(int));
+
+    for(int k = 0; k < BENCH_REPEAT; k++)
     {
-        cudaSetDevice(i);
-        num_blocks = ceil((double)colCounts[i] / ((double)num_threads/WARP_SIZE));
-        sptrsv_zerocopy_cuda_executor<<< num_blocks, num_threads >>>
-                            (d_cscColPtrTR[i], d_cscRowIdxTR[i], d_cscValTR[i],
-                                d_in_degree[i], d_left_sum[i],
-                                colCounts[i], colDispls[i], d_b[i], d_x[i], s_in_degree, s_left_sum);
+        gettimeofday(&t1, NULL);
+        
+        cudaMemset(s_left_sum, 0, n * sizeof(VALUE_TYPE));
+        memcpy(s_in_degree, s_in_degree_backup, n * sizeof(int));
+
+        for(int i = 0; i < ngpu; i++)
+        {
+            cudaSetDevice(i);
+            cudaMemset(d_x[i], 0, colCounts[i] * sizeof(VALUE_TYPE));
+            cudaMemset(d_in_degree[i], 0, colCounts[i] * sizeof(VALUE_TYPE));
+            cudaMemset(d_left_sum[i], 0, colCounts[i] * sizeof(VALUE_TYPE));
+            num_blocks = ceil((double)colCounts[i] / ((double)num_threads/WARP_SIZE));
+            sptrsv_zerocopy_cuda_executor<<< num_blocks, num_threads >>>
+                                (d_cscColPtrTR[i], d_cscRowIdxTR[i], d_cscValTR[i],
+                                    d_in_degree[i], d_left_sum[i],
+                                    colCounts[i], colDispls[i], d_b[i], d_x[i], s_in_degree, s_left_sum);
+        }
+
+        for(int i = 0; i < ngpu; i++)
+        {
+            cudaSetDevice(i);
+            cudaDeviceSynchronize();
+        }
+
+        gettimeofday(&t2, NULL);
+        time_cuda_solve += (t2.tv_sec - t1.tv_sec) * 1000.0 + (t2.tv_usec - t1.tv_usec) / 1000.0;
+
     }
 
-    // For safety, wait until all devices are done.
-    for(int i = 0; i < ngpu; i++)
-    {
-        cudaSetDevice(i);
-        cudaDeviceSynchronize();
-    }
+    time_cuda_solve /= BENCH_REPEAT;
+    double flop = 2*(double)nnzTR;
+
+    printf("cuda zerocopy SpTRSV solve used %4.2f ms, throughput is %4.2f gflops\n",
+           time_cuda_solve, flop/(1e6*time_cuda_solve));
 
     printf(" - cuda zerocopy SpTRSV solve finished!\n");
 
@@ -869,9 +908,12 @@ int sptrsv_zerocopy_cuda(const int           *cscColPtrTR,
         cudaFree(d_x[i]);
     }
 
+    cudaFree(s_in_degree);
+    cudaFree(s_left_sum);
+    cudaFree(s_in_degree_backup);
+
     return 0;
 }
-
 
 #endif
 
